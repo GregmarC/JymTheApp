0fe36fb1b4e9c6dcebafab3db089b7fc13d31721
Gregmar Cruz <gregr1201@gmail.com>
2020-03-23 14:20:39 -0400

    Netlify revision

---
+++ src/keys.js
@@ -0,0 +1,5 @@
+export const firebaseKey = "AIzaSyDcdVrvBv6R4ubgkK2Hvlu4fjthx1erpK8";
+
+export const googleKey = "AIzaSyBclbzyKKmlsbRD18JHRObpIGO6F27Q1Ao";
+
+export const stripeKey2 = "pk_test_riF4lNBnYKk2Caz9oJPJDEkg00vHb7arwy";
